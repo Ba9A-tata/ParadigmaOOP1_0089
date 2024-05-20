8a4a408365575fcf6c56a95097f4e6ba98896f9d
Ba9A-tata <yogadzakkii@gmail.com>
2024-05-20 19:24:27 +0700

    menambahkan void tambah pasen

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -14,5 +14,18 @@ public:
         cout << "Pasien \"" << nama << "\" tidak ada\n";
     }
     void tambahDokter(dokter*);
-    void cetakDokter;
+    void cetakDokter();
+};
+class dokter {
+public:
+    string nama;
+    vector<pasien*> daftar_pasien;
+
+    dokter(string pNama) :nama(pNama) {
+        cout << "Dokter \"" << nama << "\" ada\n";
+    }
+    ~dokter() {
+        cout << "Dokter \"" << nama << "\" tidak ada\n";
+    }
+    void tambahPasien(pasien*);
 };
